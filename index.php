e75a29dc3c73ff5dca9c53d6a34823f12ebb1b30
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-09 18:50:27 -0300

    testando envio de arquivos

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Uploads</title>
+    </head>
+    <body>
+        <header>
+            <h1>Uploads</h1>
+        </header>
+        <main>
+            <form method="POST" action="upfile.php" enctype="multipart/form-data">
+                <label for="nome">arquivo</label>
+                <input name="arquivoup" type="file" placeholder="Seu nome aqui" required>
+                <button type="submit" placeholder="enviar">Envie</button>
+            </form>
+        </main>
+    </body>
+</html>
---
+++ upfile.php
@@ -0,0 +1,19 @@
+<?php
+    $diretorio_upload = "enviados/";
+    try{
+        if(!is_dir($diretorio_upload))
+            throw new Exception("diretorio errado");
+        if(is_executable($_FILES["arquivoup"]["tmp_name"]))
+            throw new Exception ("Não é possivel usar esse arquivo");
+            echo "ta certo ainda";
+            //var_dump ($_FILES);
+            $extensao_arquivo = pathinfo($_FILES["arquivoup"]["name"],PATHINFO_EXTENSION);
+            $arquivo_unico = uniqid("enviado_"). "." . $extensao_arquivo;
+            $nome_arquivo_servidor = $diretorio_upload . $arquivo_unico;
+            if(move_uploaded_file($_FILES["arquivoup"]["tmp_name"],$nome_arquivo_servidor)){
+                echo "arquivo enviado";    
+            }else throw new Exception("erro" . $_FILES["arquivoup"]["error"]);
+    }catch (Exception $e){
+        echo $e -> getMessage();
+    }
+?>
